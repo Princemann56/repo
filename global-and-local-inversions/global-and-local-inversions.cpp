0b358982a064cc6776dd7b65f391fc5850f3e544
usernamecopied5656 <usernamecopied5656@gmail.com>
2023-04-16 13:56:28 +0530

    Time: 98 ms (99.44%), Space: 83.3 MB (82.6%) - LeetHub

--- global-and-local-inversions/global-and-local-inversions.cpp
+++ global-and-local-inversions/global-and-local-inversions.cpp
@@ -3,11 +3,7 @@ public:
     bool isIdealPermutation(vector<int>& nums) {
         ios::sync_with_stdio(0),std::cin.tie(0),std::cout.tie(0);
         for(int i=0; i<nums.size(); i++)
-        { 
-            if(abs(nums[i]-i)>1)
-            return false;
-
-        }
-        return true;
+            if(abs(nums[i]-i)>1) return false;
+         return true;
     }
 };
