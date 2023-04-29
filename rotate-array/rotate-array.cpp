f100d24f0d38694edfd37b5093beb7836d053d00
usernamecopied5656 <usernamecopied5656@gmail.com>
2023-04-30 00:57:24 +0530

    Time: 37 ms (25.83%), Space: 25.5 MB (40.25%) - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,33 +1,8 @@
 class Solution {
 public:
-int h=0;
-void reverse(vector<int>& nums, int low, int high)
-{
-    while(low<high)
-    {
-        swap(nums[low], nums[high]);
-        low++;
-        high--;
+    void rotate(vector<int>& nums, int k) {
+        vector<int> numcopy = nums;
+        for(int i=0;i<nums.size();i++) 
+            nums[(i+k)%nums.size()]=numcopy[i];
     }
-}
-    void rotate(vector<int>& nums, int k) 
-    {   
-        if(nums.size()==1||nums.size()==k)
-            nums;
-        if(nums.size()>k)
-        {
-        reverse(nums,nums.size()-k,nums.size()-1);
-        reverse(nums,0, nums.size()-1-k);
-        reverse(nums, 0, nums.size()-1);
-        }
-        if(nums.size()<k)
-        {
-           h=k % nums.size();
-           reverse(nums,nums.size()-h,nums.size()-1);
-        reverse(nums,0, nums.size()-1-h);
-        reverse(nums, 0, nums.size()-1);
-           
-        }
-    }
-    
 };
