dff186a7601613ae26a3956218d37d350147bfe2
usernamecopied5656 <usernamecopied5656@gmail.com>
2023-05-17 17:30:58 +0530

    Time: 0 ms (100%), Space: 6.6 MB (60.96%) - LeetHub

--- spiral-matrix-ii/spiral-matrix-ii.cpp
+++ spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -6,26 +6,26 @@ public:
         int right=n-1;int bottom=n-1;
         int a=1;
         while(top<=bottom && left<=right){
-            for(int i=left;i<=right;i++){//top
+            for(int i=left;i<=right;i++){
                 
                 res[top][i]=a;
                 a++;
             }
             top++;
-            for(int i=top;i<=bottom;i++){//right
+            for(int i=top;i<=bottom;i++){
                 res[i][right]=a;
                 a++;
             }
             right--;
             if(top<=bottom){
-                for(int i=right;i>=left;i--){//bottom in reverse
+                for(int i=right;i>=left;i--){
                 res[bottom][i]=a;
                 a++;
             }
             bottom--;
             }
             if(left<=right){
-                for(int i=bottom;i>=top;i--){//left in reverse
+                for(int i=bottom;i>=top;i--){
                 res[i][left]=a;
                 a++;
             }
