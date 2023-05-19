1211014344edeeb032604000d140d2c1261bc65a
usernamecopied5656 <usernamecopied5656@gmail.com>
2023-05-20 00:13:30 +0530

    Time: 22 ms (51.51%), Space: 19.7 MB (29.17%) - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int count=1;
+        int res=0;
+        for(int i=1;i<n; i++)
+        {
+            if(nums[res]==nums[i])
+            count++;
+            else
+            count--;
+
+            if(count==0)
+            {
+                res=i;
+                count=1;
+            }
+        }
+      return nums[res];
+    }
+};
