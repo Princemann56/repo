5c218b5413ea394ea2a8fa3440f6261471eca96c
usernamecopied5656 <usernamecopied5656@gmail.com>
2023-05-26 17:07:00 +0530

    Time: 30 ms (45.14%), Space: 24.9 MB (40.78%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,23 +1,25 @@
+
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> leftm(nums.size());
-        vector<int> rightm(nums.size());
-        vector<int> output(nums.size());
-        leftm[0]=1;
-        rightm[nums.size()-1]=1;
+       
+        vector<int> output(3*nums.size());
+        output[0]=1;
+        output[2*nums.size()-1]=1;
         for(int i=1; i<nums.size(); i++)
         {
-            leftm[i]=nums[i-1]*leftm[i-1];
+            output[i]=nums[i-1]*output[i-1];
         }
-        for(int i=nums.size()-2; i>=0; i--)
+        for(int i=(2*nums.size()-2); i>=nums.size(); i--)
         {
-            rightm[i]=rightm[i+1]*nums[i+1];
+            output[i]=output[i+1]*nums[i+1-nums.size()];
         }
-        for(int i=0; i<nums.size(); i++)
+        for(int i=(2*nums.size()); i<(3*nums.size()); i++)
         {
-            output[i]=leftm[i]*rightm[i];
+            output[i]=output[i-2*nums.size()]*output[i-nums.size()];
         }
+        output.erase(output.begin(), output.end()-nums.size());
         return output;
     }
 };
