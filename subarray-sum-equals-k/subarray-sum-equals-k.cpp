fa5dafa4347e03e7a5dd8e52169cecae0a793d79
Prince Mann <usernamecopied5656@gmail.com>
2023-06-09 15:56:52 +0530

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -8,11 +8,11 @@ public:
         m[0]=1;
         for(int i=0; i<n; i++)
         {
-           sum+=nums[i];
+           sum= sum + nums[i];
            int diff = sum-k;
 
            if(m.find(diff)!= m.end())
-           res+= m[diff];
+           res= res + m[diff];
 
            m[sum]++;
 
