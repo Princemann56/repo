56955aebee8d763bd1df4ab2ca56ea0f652768c6
Prince Mann <usernamecopied5656@gmail.com>
2023-06-24 16:14:12 +0530

    Time: 1457 ms (5.1%), Space: 339.7 MB (5.8%) - LeetHub

---
+++ combinations/combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> nums(n);
+        for(int i=0; i<n; i++)
+        nums[i]=i+1;
+        int p = 1 << n;
+       sort(nums.begin(),nums.end());
+        vector<vector<int>> subs(p);
+        vector<vector<int>> ans;
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i>>j) & (1!=0)) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+
+        for(int i=0; i<p; i++)
+        {
+            if(subs[i].size()==k)
+            ans.push_back(subs[i]);
+        }
+        return ans;
+    }
+};
