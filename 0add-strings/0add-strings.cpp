d16ade493a05c4f384d9fcf56a3aa2dd2f2694c6
Prince Mann <usernamecopied5656@gmail.com>
2023-06-26 01:04:04 +0530

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- 0add-strings/0add-strings.cpp
+++ 0add-strings/0add-strings.cpp
@@ -1,20 +1,58 @@
 class Solution {
 public:
-string addStrings(string num1, string num2) {
-    int i = num1.size() - 1;
-    int j = num2.size() - 1;
-    int carry = 0;
-    string res = "";
-    while(i>=0 || j>=0 || carry){
-        long sum = 0;
-        if(i >= 0){sum += (num1[i] - '0');i--;}
-        if(j >= 0){sum += (num2[j] - '0');j--;}
-        sum += carry; 
-        carry = sum / 10;
-        sum = sum % 10;
-        res =  res + to_string(sum);
+    string addStrings(string num1, string num2) {
+        int n1= num1.size()-1;
+        int n2= num2.size()-1;
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        int carry=0;
+        vector<int> sum;
+        int i=0;
+      
+        while(i <= max(n1,n2))
+        {   
+            int x=0;
+            if(i>n1 && i<= n2)
+            x= (num2[i]-'0') + carry;
+            else if(i>n2 && i<= n1)
+            x= (num1[i]-'0')  + carry;
+            else
+            x= (num1[i]-'0') + (num2[i]-48) + carry;
+           
+
+            if(x<= 9)
+           {   
+             
+             sum.push_back(x);
+             i++;
+             carry =0;
+           }
+           else
+           {
+               carry=1;
+               x= x % 10;
+               if(i==max(n1,n2))
+               {
+                   sum.push_back(x);
+                   sum.push_back(1);
+               }
+            else
+             sum.push_back(x);
+               i++;
+
+           }
+           
+            
+        }
+        for(auto i: sum)
+        cout<< i<<" ";
+        reverse(sum.begin(), sum.end());
+        
+        string str="";
+        
+        for(auto i: sum)
+        str= str + to_string(i);
+        
+        return str;
     }
-    reverse(res.begin(), res.end());
-    return res;
-}
 };
