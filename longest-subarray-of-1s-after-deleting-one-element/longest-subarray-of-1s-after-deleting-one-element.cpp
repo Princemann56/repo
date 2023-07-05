6ba61fbbb66333f08a50e4b3d23abe5306b39f66
Prince Mann <usernamecopied5656@gmail.com>
2023-07-05 10:41:50 +0530

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
-        int longestSubarray(vector<int>& A) {
-        int i = 0, j, k = 1;
-        for (j = 0; j < A.size(); ++j) {
-            if (A[j] == 0) k--;
-            if (k < 0 && A[i++] == 0) k++;
+    int longestSubarray(vector<int>& nums) {
+        int n= nums.size();
+        int k=1;
+        int j=0;
+        int result=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==0)
+            k--;
+            while(k<0)
+            {
+                if(nums[j]==0)
+                k++;
+                j++;
+            }
+         result = max(result, i-j);
         }
-        return j - i - 1;
+
+        return result;
     }
 };
